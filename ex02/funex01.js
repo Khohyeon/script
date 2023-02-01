c7a420da79714b581117a57e71ed68d5d5732d67
kim ho hyeon <khh5762@naver.com>
2023-02-01 15:08:28 +0900

    script Dom

---
+++ ex02/funex01.js
@@ -0,0 +1,16 @@
+function m1() {
+
+}
+function m2() {
+    return 2;
+}
+function m3(a, b) {
+    console.log("앞에 m3");
+}
+let m4 = function () { };
+
+let m5 = () => { };  // 람다식
+let m6 = () => 1;
+let m7 = () => {
+    return 1;
+};  
